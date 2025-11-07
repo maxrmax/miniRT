e9677b017861a61de48fa10a45efc3f8b2c814e6
mring <mring@student.42heilbronn.de>
2025-11-07 17:07:05 +0100

    vec file

--- src/math/vec3.c
+++ src/math/vec3.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   vec.c                                              :+:      :+:    :+:   */
+/*   vec3.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mring <mring@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/04 17:53:44 by mring             #+#    #+#             */
-/*   Updated: 2025/11/04 18:37:52 by mring            ###   ########.fr       */
+/*   Updated: 2025/11/06 17:28:07 by mring            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,3 +48,13 @@ t_vec3	vec_normalize(t_vec3 v)
 	len = vec_length(v);
 	return ((t_vec3){v.x / len, v.y / len, v.z / len});
 }
+
+/*
+need a brdf function for light refraction on surfaces and color calculation
+Bi-Directional Reflectance Distribution Function
+the function calculates the color the ray will have after in bounces off a surface/intersects with any object
+which can happen multiple times.
+the more rays the more calculation the more calls the slower the program -> here we can optimize with logic
+
+
+*/
